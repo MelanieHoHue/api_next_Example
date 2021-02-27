f58373348229cb140d16643a6c148c46da403ec3
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-02-27 09:15:26 +0100

    initial commit and first steps

---
+++ components/profile.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+const Profile = () => (
+    <Image>
+        src="./images/profile-jpg"
+        height={291}
+        width={285}
+        alt="Melanie Holzapfel-Hüttenrauch"
+    </Image>
+)
