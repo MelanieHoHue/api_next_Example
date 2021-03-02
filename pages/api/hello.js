aed2968f9f43c4a0af4650c750a0082ba6c37e9e
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-03-02 15:15:49 +0100

    some simple http examples

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,8 @@
 export default function handler (req, res) {
-    res.status(200).json({ text: 'Hello' })
+    //res.status(200).json({ text: 'Hello' })
+    if(!req.body.name) {
+        return res.status(400).send('Name is required');
+    }
+    res.status(201).send(`Thank you ${req.body.name}`);
+    //res.send();
 }
